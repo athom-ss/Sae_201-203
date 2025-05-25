493e72aa9d15929da4687e0024a825c5c3ee3fa4
BouquetEthan <bouquetethan@gmail.com>
2025-05-25 22:48:42 +0200

    Update compte.php
    lien non existant

--- sae_201_203/php/compte.php
+++ sae_201_203/php/compte.php
@@ -71,7 +71,6 @@ try {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../css/style_compte.css?v=<?= time(); ?>">
     <link rel="stylesheet" href="../css/header_nav_footer.css?v=<?= time(); ?>">
-    <link rel="stylesheet" href="../css/style_compte_admin.css?v=<?= time(); ?>">
     <title>Mon compte</title>
 </head>
 
