14d75e1e08e7a18735a3258effb39526f387ec56
Liam rd <liamandouard@gmail.com>
2025-05-25 17:15:09 +0200

    Update accueil_agent.php

--- sae_201_203/php/accueil_agent.php
+++ sae_201_203/php/accueil_agent.php
@@ -7,7 +7,7 @@
     <link rel="stylesheet" href="../css/style_accueil.css?v=<?= time(); ?>">
     <link rel="stylesheet" href="../css/header_nav_footer.css?v=<?= time(); ?>">
     <link rel="stylesheet" href="../css/style_accueil_agent.css?v=<?= time(); ?>">
-    <title>Accueil du site</title>
+    <title>Accueil du site (Agent)</title>
 </head>
 
 <header>
